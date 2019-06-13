75e80f42a516c6785d6b570ca501ba1dd6374e8a
Monesh_Venkul <mvvenkul@gmail.com>
2019-06-13 13:10:48 +0530

    Update s2_10.py

--- s2_10.py
+++ s2_10.py
@@ -1,42 +1,27 @@
 
-
-
-
 import sys, string, math
-n = int(input())
-if n==10 :
-    print('yes')
-    sys.exit()
-s = str(n)
-if len(s) <= 3 :
-    if n%8 == 0 :
-        print('yes')
-        sys.exit()
-    else :
-        print('no')
-        sys.exit()
-s2 = s[-3:]
-n2 = int(s2)
-if n2 == 0 :
-    print('yes')
-    sys.exit()
-if n2%8 == 0 :
-    print('yes')
-    sys.exit()
-s2 = s[:-1]
-n2 = int(s2)
-#print(s2,n2)
-if n2%8 == 0 :
-    print('yes')
-    sys.exit()
-s2 = s[:-2] + s[-1]
-n2 = int(s2)
-if n2%8 == 0 :
-    print('yes')
-    sys.exit()
-s2 = s[:-3] + s[-2:]
-n2 = int(s2)
-if n2%8 == 0 :
-    print('yes')
-    sys.exit()
-print('no')
+
+import sys
+
+
+def minCoins(coins, m, V):
+    if (V == 0):
+        return 0
+   
+    res = sys.maxsize
+
+    
+    for i in range(0, m):
+        if (coins[i] <= V):
+            sub_res = minCoins(coins, m, V - coins[i])
+
+            if (sub_res != sys.maxsize and sub_res + 1 < res):
+                res = sub_res + 1
+    return res
+
+
+
+n,V = input().split()
+n,V = int(n), int(V)
+coins = [ int(x) for x in input().split()]
+print( minCoins(coins, n, V))
