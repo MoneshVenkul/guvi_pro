85323e8c8b3772b3a12d75fe337f5b8e50bae70f
Monesh_Venkul <mvvenkul@gmail.com>
2019-07-12 16:56:29 +0530

    Update s1_7.py

--- s1_7.py
+++ s1_7.py
@@ -13,18 +13,4 @@ else:
 	#print(a)
 	b=a*2
 	#print(b)
-	print(n-b)import math
-n=int(input())
-l=math.log10(n)/math.log10(2)
-#print(l)
-
-#print(math.ceil(l))
-#print(math.floor(l))
-if math.ceil(l)==math.floor(l):
-	print(0)
-else:
-	a=(n-1)//2
-	#print(a)
-	b=a*2
-	#print(b)
 	print(n-b)
